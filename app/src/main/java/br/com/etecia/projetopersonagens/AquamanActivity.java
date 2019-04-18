9cbc5c86ce13d96778613c314ea72de51054afc3
Paulo Henrique <paulo.oliveira438@etec.sp.gov.br>
2019-04-17 22:09:54 -0300

    Criei a Classe e o Layout para o personagem Aquaman e crei o metodo Onclick no mesmo personagem, alem de ter criado strings para a biografia de cada personagem.

---
+++ app/src/main/java/br/com/etecia/projetopersonagens/AquamanActivity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.projetopersonagens;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AquamanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aquaman_layout);
+    }
+}
--- app/src/main/java/br/com/etecia/projetopersonagens/menuPrincipalActivity.java
+++ app/src/main/java/br/com/etecia/projetopersonagens/menuPrincipalActivity.java
@@ -1,7 +1,9 @@
 package br.com.etecia.projetopersonagens;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class menuPrincipalActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class menuPrincipalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menuprincipal_layout);
+    }
+
+    public void abriPersonagem(View view) {
+
+        switch (view.getId()){
+            case R.id.idAquaman:
+                Intent intent = new Intent(menuPrincipalActivity.this,AquamanActivity.class);
+                startActivity(intent);
+                break;
+
+        }
     }
 }
